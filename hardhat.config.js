5aaef7359acd84ecd3ab0c2f1dc29fd7d8be181f
Luis Berenguer <luis01developer@gmail.com>
2021-12-13 12:28:50 +0100

    despliegue a la red de rinkeby

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const proyectId = process.env.INFURA_PROYECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${proyectId}`,
+      accounts: [privateKey],
+    },
+  },
+};
